6818164f41b9f49414bb70a2cb6981a9fd6b0af5
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-05-31 10:58:44 +0300

    [FIX] DEBITING FUNDS

--- binance_bot/addition/sripts/debiting_funds.py
+++ binance_bot/addition/sripts/debiting_funds.py
@@ -1,49 +1,41 @@
 import asyncio
-import typing
-import decimal
-from datetime import datetime
 
-from addition.config import logger, percentage_of_pnl, decimals
+from futuresboard.blueprint import DB
 from addition.tg_bot.send_to_bot import send_to_user_bot
-from addition.db_wallet import (
-    get_users, get_profit_today_by_api_label, inset_new_balance,
-    get_api_label_list_by_user_id, get_user_balance, inset_withdraw
-)
+from addition.config import logger, percentage_of_pnl, decimals
 
-async def get_realised_pnl_for_today(api_label: str) -> typing.Union[decimal.Decimal, int]:
-    profit: decimal.Decimal = get_profit_today_by_api_label(api_label=api_label)
-    if profit >= 0:
-        return profit
-    return 0
+def withdraw_from_account(user, profit_today: float, new_balance: float) -> bool:
+    status_insert_withdraw: bool = DB.insert_new_withdraw(user=user, profit_today=profit_today)
+    if not status_insert_withdraw:
+        return False
+    send_to_user_bot(user_id=user.id, amount=decimals.create_decimal(profit_today), is_add=False)
+    return DB.insert_new_balance(user=user, new_balance=new_balance)
 
-def withdraw_from_account(user: typing.Dict, profit_today: decimal.Decimal) -> bool:
-    balance: decimal.Decimal = get_user_balance(user_id=user["id"])
-    balance -= profit_today
-    inset_withdraw(ins_time=int(datetime.timestamp(datetime.now())) * 1000, amount="%.8f" % profit_today, user_id=user["id"])
-    send_to_user_bot(user_id=user["id"], amount=profit_today, is_add=False)
-    return inset_new_balance(new_balance="%.8f" % balance, user_id=user["id"])
-
-async def check_user(user: typing.Dict) -> bool:
-    api_labels_list: typing.List = get_api_label_list_by_user_id(user_id=user["id"])
-    profit_today = 0
+async def check_user(user) -> bool:
+    api_labels_list = DB.get_api_labels_list(user_id=user.id)
+    if len(api_labels_list) == 0:
+        return True
+    profit_today: float = 0.0
     for api_label in api_labels_list:
-        realised_pnl = await get_realised_pnl_for_today(api_label=api_label)
+        realised_pnl = DB.get_realised_pnl_for_today(api_label=api_label, user_id=user.id)
         profit_today += realised_pnl
     if profit_today <= 0:
         return True
-    else:
-        percent_profit_today = profit_today / 100 * percentage_of_pnl
-        withdraw_from_account(user=user, profit_today=decimals.create_decimal(percent_profit_today))
-        return True
+    our_percent = profit_today / 100 * percentage_of_pnl
+    logger.error((
+        f"PROFIT PER DAY: {profit_today} | OUR PERCENT: {our_percent} | "
+        f"USERNAME: {user.username} | BALANCE: {user.budget}"
+    ))
+    return withdraw_from_account(user=user, profit_today=our_percent, new_balance=float(user.budget) - our_percent)
 
 async def run():
-    users = get_users()
-    logger.error("START debiting_funds.py")
+    users = DB.get_users()
+    logger.error("\nSTART DEBITING FUNDS!")
     await asyncio.gather(*[
         check_user(user)
         for user in users
     ])
-    logger.error("END debiting_funds.py")
+    logger.error("END DEBITING FUNDS\n")
 
 def main():
     asyncio.run(run())
